8a9cde300ad2b5d19af9d884c61b1f8617f7caa7
victtor hugo <hgloureirocosta@gmail.com>
2021-03-23 09:34:45 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/vaccine/vaccine/VaccineApplication.java
@@ -0,0 +1,13 @@
+package br.com.vaccine.vaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccineApplication.class, args);
+    }
+
+}
